9d2a9c8565dfc84e682084f84f926d976e4b11b5
IanCStewart <ian.c.stewart@icloud.com>
2022-10-20 12:12:29 +0200

    Translated 404 page.

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,13 +6,13 @@ import Layout from '../components/Layout';
 const NotFoundPage = () => (
   <Layout pageTitle="404">
     <article>
-      <h1>404 - Pagina niet gevonden</h1>
+      <h1>404 - Page not Found</h1>
       <p>
         Sorry{' '}
         <span role="img" aria-label="Pensive emoji">
           😔
         </span>{' '}
-        Je hebt geprobeerd een pagina te bereiken die niet (meer) lijkt te bestaan. Gebruik de bovenstaande navigatie om naar een pagina te gaan of ga terug naar de <Link to="/">homepagina</Link>.
+        , You tried to reach a page that doesn&apos;t seem to exist. Navigate to a different page or go back to the <Link to="/">home page</Link>.
       </p>
     </article>
   </Layout>
